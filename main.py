f5df76dd32b959e4548a94955d4976f4e169562f
kingdog2 <popo033284079@gmail.com>
2025-12-18 18:22:45 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("daddaw") 
+# echo "# gitTest2" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/kingdog2/gitTest2.git
+# git push -u origin main
